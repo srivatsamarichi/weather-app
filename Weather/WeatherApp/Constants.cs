1f116922413310031cbd411d3323b1fcc67782b7
Srivatsa <44693716+iamsrivatsa@users.noreply.github.com>
2020-04-13 19:09:35 +0530

    updated packages and API key

--- Weather/WeatherApp/Constants.cs
+++ Weather/WeatherApp/Constants.cs
@@ -3,6 +3,6 @@
     public static class Constants
     {
         public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-        public static string OpenWeatherMapAPIKey = "INSERT_API_KEY_HERE";
+        public static string OpenWeatherMapAPIKey = "93b271871eef835c2377ad08b524ec4b";
     }
 }
