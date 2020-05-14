8c45affc3f572c97bbd893437ac99a23c7232b44
DarqueKnight <63053071+DarqueKnight@users.noreply.github.com>
2020-05-14 16:07:26 +0530

    Update AssemblyInfo.cs
    Added a test comment

--- Weather/WeatherApp.Android/Properties/AssemblyInfo.cs
+++ Weather/WeatherApp.Android/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
-
+//Testing the App frUI test
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
